6b332bb97e770d058988ab67885ef3593e7c55c8
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2022-04-20 19:17:22 +0300

    HelioEarn

--- contracts/DAOInteraction.sol
+++ contracts/DAOInteraction.sol
@@ -204,6 +204,14 @@ contract DAOInteraction is Initializable, UUPSUpgradeable, OwnableUpgradeable {
         jug.drip(collateralType.ilk);
     }
 
+
+    function enterEarn(uint256 amount) external {
+        usb.transferFrom(msg.sender, address(this), amount);
+
+
+        emit EnteredEarn(msg.sender, amount);
+    }
+
     //    /////////////////////////////////
     //    //// VIEW                    ////
     //    /////////////////////////////////

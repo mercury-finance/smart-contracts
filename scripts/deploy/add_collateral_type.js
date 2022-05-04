6e3d5454ed91da3634dd8977432dd9ade5e5f554
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2022-05-04 19:08:25 +0300

    Add ceToken address, fix add collateral script

--- scripts/deploy/add_collateral_type.js
+++ scripts/deploy/add_collateral_type.js
@@ -14,16 +14,23 @@ const { VAT,
     INTERACTION} = require('../../addresses.json');
 const {ethers} = require("hardhat");
 
+let wad = "000000000000000000", // 18 Decimals
+    ray = "000000000000000000000000000", // 27 Decimals
+    rad = "000000000000000000000000000000000000000000000"; // 45 Decimals
+
 async function main() {
     console.log('Running deploy script');
 
     let newCollateral = ethers.utils.formatBytes32String("ceToken");
+    console.log("CeToken ilk: " + newCollateral);
+
     let tokenAddress = "0xCa33FBAb46a05D7f8e3151975543a3a1f7463F63";
 
     this.Vat = await hre.ethers.getContractFactory("Vat");
     this.Clip = await hre.ethers.getContractFactory("Clipper");
     this.Interaction = await hre.ethers.getContractFactory("DAOInteraction");
     this.GemJoin = await hre.ethers.getContractFactory("GemJoin");
+    this.Spot = await hre.ethers.getContractFactory("Spotter");
 
     const clip = await this.Clip.deploy(VAT, SPOT, DOG, newCollateral);
     await clip.deployed();
@@ -35,14 +42,21 @@ async function main() {
 
     let interaction = this.Interaction.attach(INTERACTION);
 
-    // await interaction.setCollateralType(tokenAddress, tokenJoin.address, newCollateral, clip.address);
-    await interaction.enableCollateralType(tokenAddress, tokenJoin.address, newCollateral, clip.address);
+    await interaction.setCollateralType(tokenAddress, tokenJoin.address, newCollateral, clip.address);
+    // await interaction.enableCollateralType(tokenAddress, tokenJoin.address, newCollateral, clip.address);
 
     let vat = this.Vat.attach(VAT);
     await vat.rely(tokenJoin.address);
-    // Clip deployed to: 0xAB5841e0325C621056f9DC85b1dB930FA073c424
-    // tokenJoin deployed to: 0x64508e93118394477334a2C32e03a6EEeFeab42f
-    // Finished
+    await vat["file(bytes32,bytes32,uint256)"](newCollateral, ethers.utils.formatBytes32String("line"), "50000000" + rad);
+    await vat["file(bytes32,bytes32,uint256)"](newCollateral, ethers.utils.formatBytes32String("dust"), "100000000000000000" + ray);
+
+    let spot = this.Spot.attach(SPOT);
+    await spot["file(bytes32,bytes32,address)"](newCollateral, ethers.utils.formatBytes32String("pip"), Oracle);
+    await spot["file(bytes32,bytes32,uint256)"](newCollateral, ethers.utils.formatBytes32String("mat"), "1250000000000000000000000000"); // Liquidation Ratio
+    await spot.poke(newCollateral);
+
+    // Clip deployed to: 0x830813216Fcd48f0Fe31a8e7Cc5306c272474ee6
+    // tokenJoin deployed to: 0x1E81E9F6f8a0b499104721664f87F36c11eDaB98
 
     console.log('Finished');
 }
